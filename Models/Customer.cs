2a0a2c2dc5e993bd13f84596e0d31740a6d7bc9e
HoangCoder16 <dtc2054802010244@ictu.edu.vn>
2022-10-08 09:45:41 +0700

    Create Customer model

---
+++ Models/Customer.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+namespace Models
+{
+    public class Customer : Person, IComparable<Customer>
+    {
+        public string CustomerType { get; set; }
+        public int Point { get; set; }
+        public DateTime CreateTime { get; set; }
+        public string Email { get; set; }
+        public string PathCustomerPicture { get; set; }
+        public Customer() { }
+
+        public Customer(string customerType, int point, DateTime createTime, string email,string pathCustomerPicture)
+        {
+            CustomerType = customerType;
+            Point = point;
+            CreateTime = createTime;
+            Email = email;
+            PathCustomerPicture = pathCustomerPicture;
+        }
+
+        public Customer(string personId, string fullName, DateTime birthDate,
+            string address, string phoneNumber, string customerType,
+            int point, DateTime createTime, string email,string pathCustomerPicture) :
+            base(personId, fullName, birthDate, address, phoneNumber)
+        {
+            CustomerType = customerType;
+            Point = point;
+            CreateTime = createTime;
+            Email = email;
+            PathCustomerPicture = pathCustomerPicture;
+        }
+
+        public override bool Equals(object obj)
+        {
+            return obj is Customer customer &&
+                   base.Equals(obj) &&
+                   PersonId == customer.PersonId;
+        }
+
+        public override int GetHashCode()
+        {
+            int hashCode = 2079290131;
+            hashCode = hashCode * -1521134295 + base.GetHashCode();
+            hashCode = hashCode * -1521134295 + EqualityComparer<string>.Default.GetHashCode(PersonId);
+            return hashCode;
+        }
+
+        public int CompareTo(Customer other)
+        {
+            return PersonId.CompareTo(other.PersonId);
+        }
+    }
+}
