aef3f1c8417729fa0a251ca64f1af256db4ad6cb
HoangCoder16 <dtc2054802010244@ictu.edu.vn>
2022-10-20 20:49:49 +0700

    Fix Login Splash

---
+++ SalesApp/LoginAcount.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SalesApp
+{
+    public partial class LoginAcount : Form
+    {
+        public LoginAcount()
+        {
+            InitializeComponent();
+            CenterToScreen();
+        }
+
+        private void buttonSignIn_Click(object sender, EventArgs e)
+        {
+            if (textBoxPassword.Text != "123456")
+            {
+                MessageBox.Show("PassWord không đúng !!");
+            } else
+            {
+                this.Close();
+            }
+        }
+
+        private void textBoxAccount_TextChanged(object sender, EventArgs e)
+        {
+            buttonSignIn.Enabled = false;
+            if (textBoxAccount.Text.Length == 0)
+            {
+                this.errorProvider.SetError(textBoxAccount, "You must enter data (A)");
+            }
+
+            else if (textBoxPassword.Text.Length == 0)
+            {
+                this.errorProvider.SetError(textBoxPassword, "You must enter data (B)");
+            }
+            else
+            {
+                this.errorProvider.Clear();
+                buttonSignIn.Enabled = true;
+            }
+        }
+    }
+}
---
+++ SalesApp/Splash.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SalesApp
+{
+    public partial class Splash : Form
+    {
+        public Splash()
+        {
+            InitializeComponent();
+            CenterToScreen();
+            timer1.Start();          
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            progressBar1.PerformStep();
+            if (progressBar1.Value == 100)
+            {
+                timer1.Stop();
+                HomeFrm homeFrm = new HomeFrm();
+                homeFrm.Show();
+                this.Close();
+            }
+        }
+    }
+}
